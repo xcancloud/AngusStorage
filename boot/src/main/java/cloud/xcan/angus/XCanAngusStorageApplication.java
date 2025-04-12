d467f5c916d5f9e7be6b154f47ea12fb7e7f88d7
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-04-12 22:18:04 +0800

    Fix application configuration

--- boot/src/main/java/cloud/xcan/angus/XCanAngusStorageApplication.java
+++ boot/src/main/java/cloud/xcan/angus/XCanAngusStorageApplication.java
@@ -1,14 +1,16 @@
 package cloud.xcan.angus;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Slf4j
 @EnableScheduling
-@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {
+    "cloud.xcan.angus.api.storage",
+    "cloud.xcan.angus.api.gm",
+    "cloud.xcan.angus.security"
+})
 @SpringBootApplication
 public class XCanAngusStorageApplication {
 
